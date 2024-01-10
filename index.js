be96bc427316f0f5814e28e70ed2f0e27eb615f3
MungaiS <thukusteven@gmail.com>
2024-01-10 09:30:43 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Add your code here
+function submitData(userName,userEmail){
+  const formData = {
+    name:userName,
+    email:userEmail
+  };
+
+  const configurationObject = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify(formData),
+  };
+  fetch("http://localhost:3000/users", configurationObject)
+  .then( response =>  response.json())
+  .then(object => console.log(object))
+  .catch(function (error) {
+    alert("There is an Error");
+    console.log(error.message);
+  });
+}
+ submitData ("Steve","steve@steve.com")
+ submitData("Sam","sam@sam.com")
